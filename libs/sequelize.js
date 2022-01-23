98b6acf35650b2ee7a35bb5fdcbf7b2cf7b5700c
hewlettpackard <hewlettpackard@PB5DDFB.siesa.com>
2022-01-23 00:20:26 -0500

    Modificando una entidad, modificacion sequelize-js

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,17 +5,13 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
-  //dialect: 'postgres',
+  dialect: 'postgres',
   logging: true,
 });
 
 setupModels(sequelize);
-
-sequelize.sync();
 
 module.exports = sequelize;
